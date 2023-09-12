47b9919fa6791685ef86fdc108132d7f64ca2c4b
Freddy Hidalgo-Monchez <freddy.hidalgo.m@gmail.com>
2023-09-12 15:42:05 -0400

    feat(test): add morning entry

--- e2e/home.test.js
+++ e2e/home.test.js
@@ -10,8 +10,12 @@ test('Site header contains name of project', async ({ page }) => {
   expect(await page.innerText('header')).toBe('MyRepoStats');
 });
 
-// test check body contains subtitle text
 test('Body contains subtitle question', async ({ page }) => {
   await page.goto('/');
   expect(await page.innerText('body')).toContain('When are commits typically made during the day?');
+});
+
+test('Body contains morning entry', async ({ page }) => {
+  await page.goto('/');
+  expect(await page.innerText('body')).toContain('Morning:');
 });
