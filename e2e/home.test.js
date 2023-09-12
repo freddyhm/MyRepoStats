ad51e041bbe4aadaef5b7d0107b0d2926d38aa12
Freddy Hidalgo-Monchez <freddy.hidalgo.m@gmail.com>
2023-09-12 15:39:04 -0400

    feat(test): add subtitle

--- e2e/home.test.js
+++ e2e/home.test.js
@@ -9,3 +9,9 @@ test('Site header contains name of project', async ({ page }) => {
   await page.goto('/');
   expect(await page.innerText('header')).toBe('MyRepoStats');
 });
+
+// test check body contains subtitle text
+test('Body contains subtitle question', async ({ page }) => {
+  await page.goto('/');
+  expect(await page.innerText('body')).toContain('When are commits typically made during the day?');
+});
