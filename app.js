8180cf936a752392244d98d909bd9c8cd9696b9c
Freddy Hidalgo-Monchez <freddy.hidalgo.m@gmail.com>
2023-09-13 12:30:01 -0400

    feat(github): get commits from github

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 const express = require('express')
-const handlebars = require('express-handlebars');
+const handlebars = require('express-handlebars')
+const axios = require('axios')
 
-const path = require('path')
 const app = express()
 const port = 3000
 
+async function fetchGitHubData(){
+    try {
 
+      const response = await axios.get(`https://api.github.com/repos/freddyhm/MyRepoStats/commits?per_page=10`)
+      
+      if (response.status === 200) {
+        return response.data
+      } else {
+        throw new Error(`Request failed with status: ${response.status}`)
+      }
+    } catch (error) {
+      throw error;
+    }
+}
 
-app.engine('handlebars', handlebars.engine());
+async function main() {
+    try {
+        let data = await fetchGitHubData()
+    } catch (error) {
+        console.error('Error:', error.message)
+    }
+}
+
+main()
+
+app.engine('handlebars', handlebars.engine())
 app.set("view engine", "handlebars")
 app.set('views', './views');
 
