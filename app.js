3c0163e12cdfd425faa2247dc7f0c426c0a7b29e
Freddy Hidalgo-Monchez <freddy.hidalgo.m@gmail.com>
2023-09-13 11:59:50 -0400

    feat(templating): separate into layout and views

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express')
+const handlebars = require('express-handlebars');
+
 const path = require('path')
 const app = express()
 const port = 3000
 
+
+
+app.engine('handlebars', handlebars.engine());
+app.set("view engine", "handlebars")
+app.set('views', './views');
+
 app.get('/', (req, res) => {
-
-const filePath = path.join(__dirname, 'index.html')
-
-  res.sendFile(filePath)
+    res.render("home", {
+        pageTitle: "MyRepoStats"
+    })
 })
 
 app.listen(port, () => {
